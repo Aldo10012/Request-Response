348204999e549b6768b9cee9f8ccc6201467a869
Alberto <aldo10012@gmail.com>
2020-10-24 13:46:15 -0700

    got favorit animals to work

--- app.py
+++ app.py
@@ -11,6 +11,17 @@ def homepage():
     """Shows a greating to the User."""
     return 'Are you there, world? It\'s me, Ducky!'
 
+@app.route('/penguins')
+def homepage_penguins():
+    """Shows my favorit animal."""
+    return 'Penguins are cute!'
+
+@app.route('/dogs')
+def homepage_dogs():
+    """Shows my favorit animal."""
+    return 'Dogs are the coolest! They are cute, playful, and loyal friends. '
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
