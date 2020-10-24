dd2ce7b69e3f35bd425fdbeeb442785e00fd4c1a
Alberto <aldo10012@gmail.com>
2020-10-24 14:16:43 -0700

    got Dessert, Madlib, and multiply 2 nums to work

--- app.py
+++ app.py
@@ -26,7 +26,20 @@ def favorite_animal(users_animal):
     """Display a message to the user that changes based on their favorite animal."""
     return f'Wow, {users_animal} is my favorite animal, too!'
 
+@app.route('/dessert/<users_dessert>')
+def favorite_desert(users_dessert):
+    """ Display a message to the user based on what dessert they like. """
+    return f'How did I know you liked {users_dessert}'
 
+@app.route('/madlibs/<adjective>/<noun>')
+def madlibs(adjective, noun):
+    """ Display a madlib """
+    return f'Petter was camping in the woods. There, he say a {noun} that was pritty {adjective}.'
+
+@app.route('/multiply/<num1>/<num2>')
+def multiply_2_nums(num1,num2):
+    """Displays teh product of 2 numbers"""
+    return f'{num1} times {num2} is {int(num1) * int(num2) }'
 
 
 if __name__ == '__main__':
